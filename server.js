04d6e248be08b1575c4da1bf6bf876e8e6dc9347
Peter <peter@redrabbits.com>
2016-08-16 18:31:38 +0200

    added express.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var express = require('express'),
+	port = process.env.port || 1337,
+	app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port 3000!');
+});
